3ff55770121e486b45ca873c367a1970dc16d6f9
Jeffrey DiVincent <43482496+jeffreydivi@users.noreply.github.com>
2021-03-13 15:56:48 -0500

    Bull riding UI

--- frontend/index.js
+++ frontend/index.js
@@ -1,3 +1,7 @@
+// This IS a hackathon. So hacky globals ensue.
+let offsetLeft = 0;
+let offsetRight = 0;
+
 window.onload = () => {
     // Initialize buttons
     // I opted to use a For loop to get ALL the buttons and make them navigatable.
@@ -15,4 +19,37 @@ window.onload = () => {
 function nav(pageName) {
     const pages = document.querySelector(".page.active").classList.remove("active");
     document.querySelector(`.page#${pageName}`).classList.add("active");
+}
+
+// Triggers a random movement event and update the UI to accomodate.
+function triggerMovement() {
+    // The state is one of eight possible 
+    const state = Math.floor(Math.random() * 10) % 8;
+    switch (state) {
+        // Bull moves N.
+        case 0:
+            break;
+        // Bull moves NE.
+        case 1:
+            break;
+        // Bull moves E.
+        case 2:
+            break;
+        // Bull moves SE.
+        case 3:
+            break;
+        // Bull moves S.
+        case 4:
+            break;
+        // Bull moves SW.
+        case 5:
+            break;
+        // Bull moves W.
+        case 6:
+            break;
+        // Bull moves NW.
+        case 7:
+            break;
+
+    }
 }
