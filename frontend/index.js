942255656e2158086fc2db7acd7fe2107860c9b5
Jeffrey DiVincent <43482496+jeffreydivi@users.noreply.github.com>
2021-03-13 17:35:18 -0500

    Game is mostly done.

--- frontend/index.js
+++ frontend/index.js
@@ -1,8 +1,27 @@
 // This IS a hackathon. So hacky globals ensue.
-let offsetLeft = 0;
-let offsetRight = 0;
+let offsetVert = 0;
+let offsetHoriz = 0;
+let didGyroAsk = false;
+let calVert = 0;
+let calHoriz = 0;
+
+let bullOffsetVert = 0;
+let bullOffsetHoriz = 0;
+
+const sensitivity = 5;
+const platformSize = 35;
+
+let currentTime = 0;
+
+let isPlaying = false;
 
 window.onload = () => {
+    // High Score Counter
+    if (Number(window.localStorage.getItem("hiScore")))
+        document.getElementById("hiCount").innerText = `${window.localStorage.getItem("hiScore")} Seconds`;
+    else
+    document.getElementById("hiCount").innerText = `No scores!`;
+
     // Initialize buttons
     // I opted to use a For loop to get ALL the buttons and make them navigatable.
     const allToGame = document.getElementsByClassName("toPlay")
@@ -14,42 +33,160 @@ window.onload = () => {
     for (let i = 0; i < allToScore.length; i++){
         allToScore[i].addEventListener("click", ()=>{nav("score")});
     }
+    
+    // Bull movement.
+    window.setInterval(triggerMovement, 25);
+
+    // Game-end detection
+    window.setInterval(gameEndCondition, 100);
 }
 
 function nav(pageName) {
     const pages = document.querySelector(".page.active").classList.remove("active");
     document.querySelector(`.page#${pageName}`).classList.add("active");
+
+    // Enable gyro.
+    if (pageName === "game" && !didGyroAsk) gyroRequest();
+    if (pageName === "game") isPlaying = true;
+}
+
+function gyroRequest() {
+    // Gyro request code: https://trekhleb.dev/blog/2021/gyro-web/
+    if (typeof DeviceMotionEvent.requestPermission === 'function') {
+        // Handle iOS 13+ devices.
+        DeviceMotionEvent.requestPermission().then((state) => {
+            if (state === 'granted') {
+              window.addEventListener('deviceorientation', knightTilt);
+            } else {
+              console.error('Request to access the orientation was rejected');
+            }
+          })
+          .catch(console.error);
+      } else {
+        // Handle regular non iOS 13+ devices.
+        window.addEventListener('deviceorientation', knightTilt);
+      }
+}
+
+function knightTilt(evt) {
+    // Do nothing if not in game session.
+    if (!isPlaying) return;
+
+    const vert = evt.beta;
+    const horiz = evt.gamma;
+
+    if (!didGyroAsk) {
+        // Calibration
+        calVert = vert;
+        calHoriz = horiz;
+        didGyroAsk = true;
+    }
+
+    const diffVert = vert - calVert;
+    const diffHoriz = horiz - calHoriz;
+
+    if (diffVert < -sensitivity) offsetVert++;
+    else if (diffVert > sensitivity) offsetVert--;
+
+    if (diffHoriz > sensitivity) offsetHoriz++;
+    else if (diffHoriz < -sensitivity) offsetHoriz--;
+
+    const knightEl = document.querySelectorAll(".knight");
+    for (let i = 0; i < knightEl.length; i++) {
+        knightEl[i].style.bottom = `${offsetVert}px`;
+        knightEl[i].style.left = `${offsetHoriz}px`;
+    }
 }
 
 // Triggers a random movement event and update the UI to accomodate.
 function triggerMovement() {
-    // The state is one of eight possible 
-    const state = Math.floor(Math.random() * 10) % 8;
+    if (!isPlaying) return;
+
+    // The state is one of four possible.
+    const state = Math.floor(Math.random() * 4);
+    const mag = Math.floor(Math.random() * 10);
     switch (state) {
         // Bull moves N.
         case 0:
-            break;
-        // Bull moves NE.
-        case 1:
+            bullOffsetVert += mag;
             break;
         // Bull moves E.
-        case 2:
-            break;
-        // Bull moves SE.
-        case 3:
+        case 1:
+            bullOffsetHoriz += mag;
             break;
         // Bull moves S.
-        case 4:
-            break;
-        // Bull moves SW.
-        case 5:
+        case 2:
+            bullOffsetVert -= mag;
             break;
         // Bull moves W.
-        case 6:
+        case 3:
+            bullOffsetHoriz -= mag;
             break;
-        // Bull moves NW.
-        case 7:
-            break;
+    }
 
+    const bullEl = document.querySelectorAll(".bull");
+    for (let i = 0; i < bullEl.length; i++) {
+        bullEl[i].style.bottom = `${bullOffsetVert}px`;
+        bullEl[i].style.left = `${bullOffsetHoriz}px`;
+    }
+}
+
+function gameEndCondition() {
+    // Overlap detection
+    const overlap = (Math.abs(offsetHoriz - bullOffsetHoriz) < platformSize && Math.abs(offsetVert - bullOffsetVert) < platformSize)
+    
+    if (!isPlaying) {
+        // do nothing.
+    }else if (overlap) {
+        currentTime += 0.1;
+        document.getElementById("counter").innerText = `${String((Math.floor(currentTime * 100)/100))} sec`;
+    } else {
+        // Game over
+        isPlaying = false;
+
+        // Prepare "game over" screen
+        document.getElementById("counterFin").innerText = `${String((Math.floor(currentTime * 100)/100))} sec`;
+        let wittyStr = "Damn. Maybe we should misappropaite funds for you.";
+
+        if (currentTime < 1) wittyStr = "Okay, that was depressing.";
+        else if (currentTime < 2) wittyStr = "Hey, at least you’re better than USF’s football team.";
+        else if (currentTime < 3) wittyStr = "Even Knugget the pony could do better than that.";
+        else if (currentTime < 4) wittyStr = "Hey, at least you’re better than USF’s football team.";
+        else if (currentTime < 5) wittyStr = "And you call yourself a \"national champion.\"";
+        else if (currentTime < 6) wittyStr = "Not too shabby. Did you practice at FSU?";
+        else if (currentTime < 7) wittyStr = "Maybe you should try with a Gator.";
+
+        document.getElementById("ranking").innerText = wittyStr;
+        nav("fall");
+
+        // Reset our game environment
+        bullOffsetVert = 0;
+        bullOffsetHoriz = 0;
+        offsetVert = 0;
+        offsetHoriz = 0;
+
+        const bullEl = document.querySelectorAll(".bull");
+        for (let i = 0; i < bullEl.length; i++) {
+            bullEl[i].style.bottom = `${bullOffsetVert}px`;
+            bullEl[i].style.left = `${bullOffsetHoriz}px`;
+        }
+
+        const knightEl = document.querySelectorAll(".knight");
+        for (i = 0; i < knightEl.length; i++) {
+            knightEl[i].style.bottom = `${offsetVert}px`;
+            knightEl[i].style.left = `${offsetHoriz}px`;
+        }
+
+        // High score detection
+        let currentHi = window.localStorage.getItem("hiScore");
+        if (!Number(currentHi))
+            window.localStorage.setItem("hiScore", "0");
+
+        if (Number(currentHi) < currentTime)
+            window.localStorage.setItem("hiScore", String((Math.floor(currentTime * 100)/100)));
+        
+        document.getElementById("hiCount").innerText = `${window.localStorage.getItem("hiScore")} Seconds`;
+
+        currentTime = 0;
     }
 }
