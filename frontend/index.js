44506ce19426406a1b30484e2244199d2a6b3dad
Jeffrey DiVincent <43482496+jeffreydivi@users.noreply.github.com>
2021-03-13 15:07:06 -0500

    More front-end code.

--- frontend/index.js
+++ frontend/index.js
@@ -1,4 +1,15 @@
 window.onload = () => {
+    // Initialize buttons
+    // I opted to use a For loop to get ALL the buttons and make them navigatable.
+    const allToGame = document.getElementsByClassName("toPlay")
+    for (let i = 0; i < allToGame.length; i++){
+        allToGame[i].addEventListener("click", ()=>{nav("game")});
+    }
+
+    const allToScore = document.getElementsByClassName("toScore")
+    for (let i = 0; i < allToScore.length; i++){
+        allToScore[i].addEventListener("click", ()=>{nav("score")});
+    }
 }
 
 function nav(pageName) {
